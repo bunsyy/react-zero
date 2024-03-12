4859c65e46979f214ee41a3c42696e7a124d406e
bunsy23 <bunsy2014@gmail.com>
2024-03-12 22:06:04 +0700

    fix: clean code abit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { MutableRefObject, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { AnimatePresence, motion, useInView } from "framer-motion";
-import { atom, useAtom } from "jotai";
+import { useAtom } from "jotai";
 
 import { footerVisibleHeightAtom } from "./atoms/footerAtom";
-import LongContent from "./components/long-content";
 import LoremIpsum from "./components/long-content/lorem-ipsum";
-import { useInViewport } from "./hooks/useInViewPort";
 import useNavBar from "./hooks/useNavBar";
 import { useRect } from "./hooks/useRect";
-import { LayoutProvider } from "./LayoutProvider";
 import { cn } from "./libs/cn";
 
 const Header = () => {
@@ -53,81 +50,38 @@ const Header = () => {
 };
 
 const MainBody = ({ asideTop }: { asideTop?: number }) => {
-  // console.log("🚀 ~ MainBody ~ asideTop:", asideTop);
-  const { showMainNav, showMainNavAsFixed } = useNavBar();
   const [footerVisibleHeight] = useAtom(footerVisibleHeightAtom);
-  console.log("🚀 ~ MainBody ~ footerAtom:", footerVisibleHeight);
-
-  // return (
-  //   <div
-  //     className={cn(
-  //       "flex h-[calc(100vh-50px)] bg-green-400",
-  //       !showMainNav && "bg-yellow-300",
-  //       showMainNavAsFixed && "bg-pink-300"
-  //     )}
-  //   >
-  //     <aside
-  //       className={cn(
-  //         "flex-0 h-full w-28 bg-cyan-300",
-  //         showMainNavAsFixed && "pt-40"
-  //       )}
-  //     >
-  //       ASIDE
-  //     </aside>
-  //     <main>MAIN BODY</main>
-  //   </div>
-  // );
 
   const asideHeight = asideTop + footerVisibleHeight + 1;
 
   return (
-    <div
-      className={cn(
-        "flex bg-green-400"
-        // `max-h-[calc(100vh-${asideTop}px)]`
-      )}
-      // style={{
-      //   height: `calc(100vh-${asideTop}px)`,
-      //   maxHeight: `calc(100vh-${asideTop}px)`,
-      // }}
-    >
+    <div className={cn("flex")}>
       <aside
         className={cn(
-          "flex-0 sticky h-full w-48 overflow-y-scroll",
-          showMainNav && "bg-violet-300",
-          !showMainNav && "bg-sky-300",
-          showMainNavAsFixed && "bg-orange-300"
+          "flex-0 sticky h-full w-48 overflow-y-scroll"
+          // showMainNav && "bg-violet-300",
+          // !showMainNav && "bg-sky-300",
+          // showMainNavAsFixed && "bg-orange-300"
         )}
         style={{
           height: `calc(100vh - ${asideHeight}px)`,
           maxHeight: `calc(100vh - ${asideHeight}px)`,
-          // top: asideTop && asideTop >= 48 ? asideTop : 48,
           top: asideTop && asideTop > 48 ? asideTop : 48,
         }}
       >
-        <LoremIpsum className="bg-slate-300" />
+        <LoremIpsum className="" />
       </aside>
-      <main className="w-full bg-amber-300">
+      <main className="w-full">
         <LoremIpsum />
       </main>
     </div>
   );
 };
 
-// const calculateTopPosition = (elementRef: MutableRefObject<null>) => {
-//   const rect = elementRef.current.getBoundingClientRect();
-//   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//   return rect.top + scrollTop;
-// };
-
 const MainContent = () => {
   const { isNewReleaseSticky } = useNavBar();
   const [rect, newReleaseRef] = useRect("scroll");
   const [asideTop, setAsideTop] = useState(rect?.bottom);
-  // console.log("🚀 ~ MainContent ~ newReleaseRef:", {
-  //   rect: rect,
-  //   asideTop: rect?.bottom,
-  // });
 
   useEffect(() => {
     setAsideTop(rect?.bottom);
@@ -149,7 +103,6 @@ const MainContent = () => {
         <div>NEW RELEASE</div>
         <div>FILTERS</div>
       </motion.div>
-      {/* <LongContent className="flex bg-cyan-200" /> */}
       <MainBody asideTop={asideTop} />
     </div>
   );
@@ -160,8 +113,6 @@ const Footer = () => {
   const [footerRect, footerRef] = useRect("scroll");
   const isFooterInView = useInView(footerRef);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
-  const [prevFooterVisibleHeight, setPrevFooterVisibleHeight] = useState(0);
-  const { isScrollingDown } = useNavBar();
 
   const scrollMaxY =
     document.documentElement.scrollHeight -
@@ -178,10 +129,6 @@ const Footer = () => {
   useEffect(() => {
     const handleScroll = () => {
       const curScrollPos = window.scrollY;
-
-      const curFooterVisibleHeight = isFooterInView
-        ? prevScrollPos - (scrollMaxY - footerRect?.height)
-        : 0;
 
       if (Math.ceil(curScrollPos) === scrollMaxY) {
         setFooterAtom(96);
@@ -201,7 +148,7 @@ const Footer = () => {
   });
 
   return (
-    <div ref={footerRef} className="h-24 bg-blue-300">
+    <div ref={footerRef} className="h-24">
       FOOTER
     </div>
   );
